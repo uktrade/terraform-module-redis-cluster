11546f60221c99949e33e9801f158f7df0b59872
Bowen Sun <bowensun@gmail.com>
2017-06-06 11:00:13 +0100

    - no persistent storage required for Redis

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_launch_configuration" "redis" {
   root_block_device {
     volume_type = "gp2"
     volume_size = 20
-    delete_on_termination = false
+    delete_on_termination = true
   }
   user_data = "${data.template_file.redis-cloudinit.rendered}"
   associate_public_ip_address = true
